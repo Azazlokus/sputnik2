d71bc3b540bb027c415ecc1aa0907c3c2bad011d
Azazlokus <62481032+Azazlokus@users.noreply.github.com>
2023-07-19 17:18:47 +0700

    reduce UserController and AuthController

---
+++ server/app/app/Http/Controllers/AuthController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginUserRequest;
+use App\Http\Resources\LoginResource;
+use App\Http\Resources\LogoutResource;
+use Illuminate\Support\Facades\Auth;
+use Mockery\Exception;
+
+
+class AuthController extends Controller
+{
+    public function login(LoginUserRequest $request)
+    {
+        $credentials = $request->only('email', 'password');
+        if ($token = auth()->attempt($credentials)) {
+            return new LoginResource($token);
+        }
+        throw new Exception();
+    }
+    public function logout()
+    {
+        $user = Auth::guard('api')->user();
+        Auth::guard('api')->logout();
+        return new LogoutResource($user);
+    }
+
+}
--- server/app/routes/api.php
+++ server/app/routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\NotificationController;
 use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Orion\Facades\Orion;
 
@@ -20,10 +20,8 @@ use Orion\Facades\Orion;
 Orion::resource('/users', UserController::class);
 
 Route::group(['prefix' => 'users'], function () {
-    //Route::post('/', [UserController::class, 'create']);
-    Route::post('/login', [UserController::class, 'login']);
-    Route::post('/logout', [UserController::class, 'logout']);
-    //Route::post('/index', [UserController::class, 'index']);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 })->middleware('api');
 
 Route::group(['prefix' => 'notification'], function () {
