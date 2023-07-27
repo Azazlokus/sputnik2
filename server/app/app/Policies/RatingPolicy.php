346b6bbabf71fd9fe9654ec1fe3eea88bed2e61c
Azazlokus <62481032+Azazlokus@users.noreply.github.com>
2023-07-27 18:25:37 +0700

    fix NotificationPolicy

--- server/app/app/Policies/RatingPolicy.php
+++ server/app/app/Policies/RatingPolicy.php
@@ -26,7 +26,7 @@ class RatingPolicy
     {
         return $this->allow();
     }
-    public function view(User $user, Rating $rating)
+    public function view(User $user, Rating $rating): Response
     {
         return $this->allow();
     }
